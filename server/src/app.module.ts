5adb273e211c2617ddc826e928b8cfaf2c6007cb
Denis <denis.samofalov@bk.ru>
2023-04-02 17:33:18 +0300

    server ready

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,8 +1,19 @@
 import { Module } from "@nestjs/common";
+import { MongooseModule } from "@nestjs/mongoose";
+import { ServeStaticModule } from "@nestjs/serve-static";
+import { FileModule } from "./file/file.module";
 import { TrackModule } from "./track/track.module";
+import * as path from 'path'
 
 @Module({
-  imports: [TrackModule]
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: path.resolve(__dirname, 'static'),
+    }),
+    TrackModule,
+    FileModule,
+    MongooseModule.forRoot('mongodb+srv://root:97XulU2L5wQFOtSV@cluster0.fenpd2k.mongodb.net/?retryWrites=true&w=majority')
+  ]
 })
 
 export class AppModule {}
